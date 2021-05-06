682581ccef7925fcc6c8fe1d8dead45d90ac260c
Reymerson Concepcion <reymerson.concepcion@gmail.com>
2021-05-05 16:54:03 -1000

    FizzBuzz

---
+++ 01-java-fundamentals/03-fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz checkValue = new FizzBuzz();
+        String result = checkValue.fizzBuzz(15);
+        System.out.println(result);
+    }
+}
